470e6950a47ac917bc9f5f1728cbde32e49681ae
Greg Fairbrother <greg@intrepica.com.au>
2009-06-26 16:29:15 +1000

    Initial commit

---
+++ lib/yore.rb
@@ -0,0 +1,21 @@
+# cant run commands like `history` as it's a bash builtin, i.e. `which history` returns nothing
+#puts history_file_location = `echo $HISTFILE`
+#puts ENV['HISTFILE']
+
+# so for now it's dirty, but works for me, I have to just load in
+bash_history_file = "/Users/#{`whoami`.strip}/.bash_history"
+
+history = []
+bash_history_file = File.new(bash_history_file, 'r')
+while (history_item = bash_history_file.gets)
+  history << history_item
+end
+
+puts "Search term: "
+term = gets
+
+puts "Commands containing: #{term}"
+
+# history.each do |h|
+#   puts h
+# end
