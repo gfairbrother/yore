47b9d5b739035d9a927fc6219797e753e2166c78
Greg Fairbrother <greg@intrepica.com.au>
2010-12-08 14:33:00 +1000

    imported into rubymine

--- lib/yore.rb
+++ lib/yore.rb
@@ -5,17 +5,23 @@
 # so for now it's dirty, but works for me, I have to just load in
 bash_history_file = "/Users/#{`whoami`.strip}/.bash_history"
 
+# load history items into an array
 history = []
 bash_history_file = File.new(bash_history_file, 'r')
 while (history_item = bash_history_file.gets)
   history << history_item
 end
 
-puts "Search term: "
-term = gets
+# remove duplicate entries
+history.uniq!
 
-puts "Commands containing: #{term}"
+#puts "Search term: "
+#term = gets
+#
+#puts "Commands containing: #{term}"
 
-# history.each do |h|
-#   puts h
-# end
+#history.each do |h|
+#  puts h
+#end
+
+
